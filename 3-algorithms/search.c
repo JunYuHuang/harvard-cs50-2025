fe2e9c5494052a7b910e3acc533c241eb3a5f8ef
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2025-11-24 11:46:00 -0800

    Complete notes for Week 3 Algorithms

--- 3-algorithms/search.c
+++ 3-algorithms/search.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -7,10 +8,10 @@ int main(void)
     string s =  get_string("String: ");
     for (int i = 0; i < 6; i++)
     {
-        if (strings[i] == s)
+        if (strcmp(strings[i], s) == 0)
         {
-            printf("Found\n");;
-            return 0
+            printf("Found\n");
+            return 0;
         }
     }
     printf("Not found\n");
