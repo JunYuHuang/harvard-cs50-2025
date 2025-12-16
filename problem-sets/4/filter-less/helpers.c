41058806191a9e04aef487e0910c8bfd26c4058f
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2025-12-15 22:44:20 -0800

    Start WIP on Problem Set 4 'Filter (Less)'

---
+++ problem-sets/4/filter-less/helpers.c
@@ -0,0 +1,40 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            BYTE rgb_average = (BYTE) round((
+                image[h][w].rgbtBlue +
+                image[h][w].rgbtGreen +
+                image[h][w].rgbtRed
+            ) / 3.0);
+            image[h][w].rgbtBlue = rgb_average;
+            image[h][w].rgbtGreen = rgb_average;
+            image[h][w].rgbtRed = rgb_average;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
