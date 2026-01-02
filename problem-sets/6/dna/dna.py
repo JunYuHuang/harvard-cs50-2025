180383865915f0aab26892280ebaefc236aa24a7
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2026-01-01 23:05:33 -0800

    Continue WIP on 'DNA' in Problem Set 6

--- problem-sets/6/dna/dna.py
+++ problem-sets/6/dna/dna.py
@@ -1,20 +1,52 @@
 import csv
-import sys
-
+from sys import exit, argv
 
 def main():
 
     # TODO: Check for command-line usage
+    if len(argv) != 3:
+        print("Usage: python dna.py database.csv sample.txt")
+        exit(1)
 
     # TODO: Read database file into a variable
+    strs = []
+    str_tuples_to_names = {}
+    with open(argv[1]) as database_file:
+        reader = csv.DictReader(database_file)
+        strs = list(reader.fieldnames)
+
+        # remove the first value `name` of the CSV table header 
+        strs.pop(0)
+
+        for row in reader:
+            name = row[0]
+            str_counts = []
+
+            row_len = len(row)
+            for str_count in range(1, row_len):
+                str_counts.append(int(str_count))
+
+            # add the (int tuple, string) key-value pair to the hashmap
+            str_tuples_to_names[tuple(str_counts)] = name
     
     # TODO: Read DNA sequence file into a variable
+    sample = ""
+    with open(argv[2], "r") as sample_file:
+        sample = sample_file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    sample_str_counts = []
+    for str in strs:
+        sample_str_counts.append(longest_match(sample, str))
 
     # TODO: Check database for matching profiles
+    sample_str_counts = tuple(sample_str_counts)
+    if (str_tuples_to_names.get(sample_str_counts)):
+        print(str_tuples_to_names[sample_str_counts])
+    else:
+        print("No match")
 
-    return
+    exit(0)
 
 
 def longest_match(sequence, subsequence):
